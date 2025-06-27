148c1fe84a24cc096e009fcb7d059d2e2abdfbeb
Brian <jorgebrianvdv@gmail.com>
2025-06-27 15:49:58 -0500

    feat: agregado lima cargo

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -1,20 +1,22 @@
-import { useEffect, useRef } from 'react';
-import '../styles/Clientes.scss'; // Archivo SCSS
-import cliente1 from '../assets/img/clientes/cliente1.jpg';
-import cliente2 from '../assets/img/clientes/cliente2.png';
-import cliente3 from '../assets/img/clientes/cliente3.jpg';
-import cliente4 from '../assets/img/clientes/cliente4.jpg';
-import cliente5 from '../assets/img/clientes/cliente6.jpg';
-import cliente6 from '../assets/img/clientes/cliente7.png';
+import { useEffect, useRef } from "react";
+import "../styles/Clientes.scss"; // Archivo SCSS
+import cliente1 from "../assets/img/clientes/cliente1.jpg";
+import cliente2 from "../assets/img/clientes/cliente2.png";
+import cliente3 from "../assets/img/clientes/cliente3.jpg";
+import cliente4 from "../assets/img/clientes/cliente4.jpg";
+import cliente5 from "../assets/img/clientes/cliente5.jpg";
+import cliente6 from "../assets/img/clientes/cliente6.jpg";
+import cliente7 from "../assets/img/clientes/cliente7.png";
 
 const ClientesTrompo = () => {
   const clientes = [
-    { id: 1, logo: cliente1, alt: 'Cliente 1' },
-    { id: 2, logo: cliente2, alt: 'Cliente 2' },
-    { id: 3, logo: cliente3, alt: 'Cliente 3' },
-    { id: 4, logo: cliente4, alt: 'Cliente 4' },
-    { id: 5, logo: cliente5, alt: 'Cliente 5' },
-    { id: 6, logo: cliente6, alt: 'Cliente 6' },
+    { id: 1, logo: cliente1, alt: "Cliente 1" },
+    { id: 2, logo: cliente2, alt: "Cliente 2" },
+    { id: 3, logo: cliente3, alt: "Cliente 3" },
+    { id: 4, logo: cliente4, alt: "Cliente 4" },
+    { id: 5, logo: cliente5, alt: "Cliente 5" },
+    { id: 6, logo: cliente6, alt: "Cliente 6" },
+    { id: 7, logo: cliente7, alt: "Cliente 7" },
   ];
 
   return (
@@ -29,16 +31,8 @@ const ClientesTrompo = () => {
             <div className="clientes-trompo__stage">
               <div className="clientes-trompo__carousel">
                 {clientes.concat(clientes).map((cliente, index) => (
-                  <div
-                    key={cliente.id}
-                    className="clientes-trompo__item"
-                  >
-                    <img
-                      src={cliente.logo}
-                      alt={cliente.alt}
-                      className="clientes-trompo__logo"
-                      loading="lazy"
-                    />
+                  <div key={cliente.id} className="clientes-trompo__item">
+                    <img src={cliente.logo} alt={cliente.alt} className="clientes-trompo__logo" loading="lazy" />
                   </div>
                 ))}
               </div>
