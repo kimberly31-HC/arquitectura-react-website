3ab346f039a7dbc89b38aa8494b725185cea41cc
Brian <jorgebrianvdv@gmail.com>
2025-07-02 08:52:09 -0500

    feat: agrandar los carteles de promocion

--- src/components/NotificacionPromo.jsx
+++ src/components/NotificacionPromo.jsx
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/NotificacionPromo.scss';
-import { IoClose } from 'react-icons/io5';
+import React, { useState, useEffect } from "react";
+import "../styles/NotificacionPromo.scss";
+import { IoClose } from "react-icons/io5";
 
 const NotificacionPromo = () => {
   const [visible, setVisible] = useState(true);
 
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <>
       {visible && (
         <div className="notificacion-promo-flotante">
-          <button 
-            className="cerrar-btn" 
-            onClick={() => setVisible(false)} 
-            aria-label="Cerrar notificación"
-          >
-            <IoClose size={20} />
+          <button className="cerrar-btn" onClick={() => setVisible(false)} aria-label="Cerrar notificación">
+            <IoClose size={25} />
           </button>
-          <p>🎉 ¡Hasta fin de junio! Paga con cualquier tarjeta de crédito. 💳</p>
+          <p>🎉 ¡Hasta fin de julio! Paga con cualquier tarjeta de crédito. 💳</p>
         </div>
       )}
     </>
